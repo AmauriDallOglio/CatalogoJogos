c53ee00b4af4c15186a183a5e80ba1cd917ae3d9
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-04-01 20:15:43 -0300

    Tratamento de erro do swagger

--- CatalogoJogos.Api/Program.cs
+++ CatalogoJogos.Api/Program.cs
@@ -1,5 +1,12 @@
 
+using CatalogoJogos.Api.Controllers.V1;
+using CatalogoJogos.Api.Middleware;
+using CatalogoJogos.Nucleo.Interface;
+using CatalogoJogos.Nucleo.Repositorio;
+using CatalogoJogos.Nucleo.Service;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace CatalogoJogos.Api
 {
@@ -15,8 +22,24 @@ namespace CatalogoJogos.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogoJogos", Version = "v1" })
-            );
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogoJogos", Version = "v1" });
+
+                //propriedade da api / xml no debug de saida
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var fileName = typeof(Program).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
+            });
+
+            builder.Services.AddScoped<IJogoService, JogoService>();
+            builder.Services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
+
+
+            //http://localhost:5024/api/v1/CicloDeVidaId
+            builder.Services.AddSingleton<IExemploSingleton, ExemploCicloDeVida>();
+            builder.Services.AddScoped<IExemploScoped, ExemploCicloDeVida>();
+            builder.Services.AddTransient<IExemploTransient, ExemploCicloDeVida>();
+
 
             var app = builder.Build();
 
@@ -26,6 +49,8 @@ namespace CatalogoJogos.Api
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
---
+++ CatalogoJogos.Nucleo/Repositorio/script.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoJogos.Nucleo.Repositorio
+{
+    internal class script
+    {
+
+        //create database CatalogoJogos
+
+        //use CatalogoJogos
+
+
+        //CREATE TABLE Jogo(
+        //    Id UNIQUEIDENTIFIER PRIMARY KEY,
+        //    Nome NVARCHAR(MAX) NOT NULL,
+        //    Produtora NVARCHAR(MAX),
+        //    Preco FLOAT
+        //);
+    }
+}
